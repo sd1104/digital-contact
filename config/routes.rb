32ef3a2a4cbd458400a7ba9666e5ca9e31abb606
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-07-23 17:10:18 +0900

    routes restored to pre-text-ver

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "contact_books#index"
+  root "rooms#index"
   resources :rooms, only: :index
   resources :contact_books, only: :index
 end
