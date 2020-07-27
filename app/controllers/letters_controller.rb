87606b76d3bb88e69259c3834351de8e1235807c
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-07-27 19:14:23 +0900

    upload function implemented

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -2,20 +2,21 @@ class LettersController < ApplicationController
   before_action :set_foreign_instance
 
   def index
-    @letters = Letter.all.order("release_at ASC")
+    @letters = Letter.all.order("release_at ASC").includes(:room)
   end
 
   def new
+    @letter = Letter.new
   end
 
   def create
-    letter = Letter(params[:room_id])
-    letter.update(letter_params)
+    Letter.create(letter_params)
+    redirect_to room_letters_path(@room)
   end
 
   private
   def letter_params
-    params.require(:letters).permit(:released_at, :name, :pdf)
+    params.require(:letter).permit(:released_at, :name, :pdf).merge(room_id: params[:room_id])
   end
   
   def set_foreign_instance
