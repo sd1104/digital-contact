4da1379097c1fdb285ba80bc5ce4771fb3ed3a84
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-07-27 15:34:17 +0900

    controller settings:bofore_action done

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -1,4 +1,24 @@
 class LettersController < ApplicationController
+  before_action :set_params
+  before_action :set_foreign_instance
+
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+  def set_params
+    params.require(:letter).permit(:released_at, :name, :pdf)
+  end
+  
+  def set_foreign_instance
+    @rooms = Room.includes(:contact_books).order("number ASC")
+    @room = Room.find(params[:room_id])
+  end
+
 end
