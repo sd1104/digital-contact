2f315459a66e6789b5a5e4721335e8810595544f
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-07-28 22:52:51 +0900

    user model and room_user model and views made

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,30 @@
 class Room < ApplicationRecord
   has_many :contact_books
+  has_many :letters
+  has_many :room_users
+  has_many :users, through: :room_users
+  has_many :absent_contacts
 end
+
+
+
+# class CreateAbsentContacts < ActiveRecord::Migration[6.0]
+#   def change
+#     create_table :absent_contacts do |t|
+#       t.date :arised_at, null: false
+#       t.integer :absent_kind, null: false
+#       t.text :reason, null: false
+#       t.references :room, null:false, foreign_key: true
+#       t.timestamps
+#     end
+#   end
+# end
+
+# belongs_to :room
+
+
+# with_options presence: true do
+#   validates :arised_at
+#   validates :absent_kind
+#   validates :reason
+# end
