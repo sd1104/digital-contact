3f2d44e1722ebbb44c35a9c85489625168e0a2c7
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-07-30 19:34:52 +0900

    group_index_uploading done

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "rooms#index"
   
-  resources :rooms, only: :index do
+  resources :rooms, only: [:index, :show] do
     resources :contact_books, only: [:index, :new, :create, :show]
     resources :letters, only:[:index, :new, :create]
     resources :absent_contacts, only: [:index, :create]
