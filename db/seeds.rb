f6bb12ff1edd0fe0161b33b56f953fe72b93e834
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-07-30 11:37:19 +0900

    Data for rails db:seed_user made and done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,19 @@
 require "csv"
 
-CSV.foreach('/Users/satouhiroshi/projects/digital-contact/db/roomData.csv') do |row|
+# CSV.foreach('/Users/satouhiroshi/projects/digital-contact/db/roomData.csv') do |row|
 
-  Room.create(:number => row[0])
+#   Room.create(:number => row[0])
 
-end
+# end
 
-CSV.foreach('/Users/satouhiroshi/projects/digital-contact/db/contact_book_sampleData.csv') do |row|
+# CSV.foreach('/Users/satouhiroshi/projects/digital-contact/db/contact_book_sampleData.csv') do |row|
 
-  ContactBook.create(:date => row[0], :letter => row[1], :bring => row[2], :schedule => row[3], :homework => row[4], :inform => row[5], :room_id => row[6])
+#   ContactBook.create(:date => row[0], :letter => row[1], :bring => row[2], :schedule => row[3], :homework => row[4], :inform => row[5], :room_id => row[6])
+
+# end
+
+CSV.foreach('/Users/satouhiroshi/projects/digital-contact/db/userDataSample.csv') do |row|
+
+  User.create(:parent_name => row[0], :child_name => row[1], :email => row[2], :password => row[3])
 
 end
