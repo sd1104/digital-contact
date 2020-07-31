c73a1cef2f6a47bed86ce5287b255157d6cbf01a
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-07-31 13:58:16 +0900

    route revised for misspells

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   
   resources :rooms, only: [:index, :show] do
     resources :contact_books
-    resources :letters, only:[:index, :new, :create, :destory]
-    resources :absent_contacts, only: [:index, :create, :edit, :update, :destory]
+    resources :letters, only:[:index, :new, :create, :destroy]
+    resources :absent_contacts, only: [:index, :create, :edit, :update, :destroy]
   end
 
 end
