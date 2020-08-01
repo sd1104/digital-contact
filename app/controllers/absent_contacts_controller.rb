ad39bd244f8e338bc23a09332d938ce8ac41008f
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-08-01 18:27:46 +0900

    controller settings done: update action

--- app/controllers/absent_contacts_controller.rb
+++ app/controllers/absent_contacts_controller.rb
@@ -9,10 +9,6 @@ class AbsentContactsController < ApplicationController
     @today_absent_contact_for_parent = AbsentContact.includes(:room, :user).where(absent_at: Date.today, user_id: current_user.id, room_id: (params[:room_id])).order('absent_at DESC')
     @today_absent_contact_for_teacher = AbsentContact.includes(:room, :user).where(absent_at: Date.today, room_id: (params[:room_id])).order('absent_at DESC')
     @absent_contact = AbsentContact.new
-    respond_to do |format|
-      format.html
-      format.json
-    end
   end
 
   def create
@@ -30,6 +26,10 @@ class AbsentContactsController < ApplicationController
       redirect_to room_absent_contacts_path(@room)
     else
       render :edit
+    end
+    respond_to do |format|
+      format.html
+      format.json
     end
   end
 
