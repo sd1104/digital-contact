a5955372ae09d73b7d43dadee43d9997b3342001
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-08-09 18:57:13 +0900

    scool model made

---
+++ app/models/school.rb
@@ -0,0 +1,11 @@
+class School < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :rooms
+  has_many :users
+
+  validates :name, :school_admin, 
+    presence: true
+end
