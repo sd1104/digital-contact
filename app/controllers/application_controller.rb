68d8946634aa11afe1c1c99b9676c9be7d6c98cb
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-08-09 18:58:03 +0900

    route set

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,25 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+  def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope == :school
+      root_path
+    else
+      root_path
+    end
+  end
 
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :school
+      new_school_session_path
+    else
+      new_user_session_path
+    end
+  end
+  
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:parent_name, :child_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:parent_name, :child_name, :name, :school_admin])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :schools, controllers: {
+    sessions: 'schools/sessions',
+    registrations: 'schools/registrations'
+  }
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   root "rooms#index"
   
   resources :rooms, only: [:index, :show] do
