e3a60e64d07acafaeb79da044b747c2dd4f6d974
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-08-10 18:15:37 +0900

    route settings done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :schools
-  devise_for :users
+  devise_for :schools, controllers: {
+    sessions:      'schools/sessions',
+    passwords:     'schools/passwords',
+    registrations: 'schools/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "rooms#index"
   
